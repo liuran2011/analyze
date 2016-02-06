77566ccbbd6c87274d5c18d109aa2681a05f4df7
liuran2011 <liuran-001@126.com>
2016-02-07 07:15:51 +0800

    create UserInfoMgr instance

--- analyze/search_engine/search_engine_base.py
+++ analyze/search_engine/search_engine_base.py
@@ -1,9 +1,19 @@
 import urllib
+from user_info_mgr import UserInfoMgr
+import copy
 
 class SearchEngineBase(object):
-    def __init__(self,conf):
+    def __init__(self,conf,env):
         self.conf=conf
         self.max_page=100
+        self.env=env
+        self.user_info_mgr=UserInfoMgr(env)
+        self.user_list=[]
+
+    def user_list_reload(self):
+        self.user_info_mgr.lock()
+        self.user_list=copy.deepcopy(self.user_info_mgr.user_info())
+        self.user_info_mgr.unlock()
 
     def fetch_page(self,url):
         page=None
@@ -15,7 +25,4 @@ class SearchEngineBase(object):
         except IOError as e:
             print "open url:%s failed."%(url)
             return page
-
-    def next(self):
-        pass
 
