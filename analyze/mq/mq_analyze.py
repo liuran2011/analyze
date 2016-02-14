855907afb89fb9f9c009cad702d4be750aed6f11
liuran <liuran@fnii.cn>
2016-02-14 22:38:53 +0800

    stats send and recv msg works.

--- analyze/mq/mq_analyze.py
+++ analyze/mq/mq_analyze.py
@@ -1,26 +1,31 @@
 from kombu import Exchange,Queue,Consumer,Connection
 from constants import *
+from kombu.mixins import ConsumerMixin
 
-class AnalyzeMQ(object):
-    def __init__(self,conf):
-        self.conf=conf
-
-        self._stats_init()
+class AnalyzeConsumer(ConsumerMixin):
+    def __init__(self,connection):
+        self.connection=connection
+        self.stats_exchange=Exchange(SE_STATS_EXCHANGE,"topic",delivery_mode=1)
+        self.stats_queue=Queue(SE_STATS_QUEUE,
+                            exchange=self.stats_exchange,
+                            routing_key='*.'+SE_STATS_TOPIC_SUFFIX)
+        
+    def get_consumers(self,Consumer,channel):
+        return [Consumer(queues=[self.stats_queue],callbacks=[self._stats_msg_proc])]
 
     def _stats_msg_proc(self,body,message):
         print body
         message.ack()
 
+class AnalyzeMQ(Consumer):
+    def __init__(self,conf):
+        self.conf=conf
+
+        self._stats_init()
+
     def _stats_init(self):
         self.connect=Connection(self.conf.rabbit_connection())
-        self.stats_exchange=Exchange(SE_STATS_EXCHANGE,"topic",delivery_mode=1)
-        self.stats_queue=Queue(SE_STATS_QUEUE,
-                            exchange=self.stats_exchange,
-                            routing_key='*.'+SE_STATS_TOPIC_SUFFIX)
-        self.stats_consumer=self.connect.Consumer(self.stats_queue,callbacks=[self._stats_msg_proc])
+        self.consumer=AnalyzeConsumer(self.connect)
 
     def run(self):
-        while True:
-            print "drain event..."
-            #self.stats_consumer.consume()
-            self.connect.drain_events()
+        self.consumer.run()
