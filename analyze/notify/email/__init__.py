3b3e064a0a7ceb62b36b5280eb75158ba483ef75
liuran <liuran@fnii.cn>
2016-04-27 21:56:59 +0800

    finish email send function.

--- analyze/notify/email/__init__.py
+++ analyze/notify/email/__init__.py
@@ -1,16 +1,51 @@
 from notify.basic import BasicNotify
 from log.log import LOG
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 class Notify(BasicNotify):
     def __init__(self,db):
         super(Notify,self).__init__(db)
 
-    def send(self,report):
+    def _build_msg(self,report,sendto,sendfrom,report_format):
+        try:
+            f=open(report,'rb')
+        except IOError as e:
+            LOG.error("open report %s failed. except:%s"%(report,e))
+            return None
+
+        msg=MIMEMultipart()
+        attach=MIMEText(f.read(),'base64','utf-8')
+        attach['Content-Type']='application/octet-stream'
+        attach['Content-Disposition']='attachment; filename="报告.%s"'%(report_format)
+        msg.attach(attach)
+        
+        msg['to']=sendto
+        msg['from']=sendfrom
+        msg['subject']='报告'
+
+        return msg
+
+    def send(self,report,report_format,sendto):
         setting=self.db.global_setting()
         if not setting:
             LOG.error('email not set')
-            return
+            return False
+       
+        msg=self._build_msg(report,sendto,setting.email,report_format)
+        if not msg:
+            return False
 
-        smtp=smtplib.SMTP()    
-        smtp
+        try:
+            smtp=smtplib.SMTP()    
+            smtp.connect(setting.smtp_host,setting.smtp_port)
+            smtp.login(setting.smtp_username,setting.smtp_password)
+            smtp.sendmail(setting.email,sendto,msg.as_string())
+            smtp.close()
+
+            return True
+
+        except Exception as e:
+            LOG.error("send mail to %s failed."%(sendto))
+            return False
